cf0ac9f01625ebe3b56c1b8b1a0b263dcf878433
Mayank Gour <141131861+Mayankg3455@users.noreply.github.com>
2024-03-17 19:29:03 +0530

    again

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using DotNetCoreSqlDb.Data;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 
-services.AddStackExchangeRedisCache(options =>
+Services.AddStackExchangeRedisCache(options =>
 {
     options.InstanceName = Configuration.GetValue<string>("SampleInstance");
     options.ConfigurationOptions = new ConfigurationOptions()
